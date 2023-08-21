f26cbc34ccb181194c3949c08505b483d9773c76
Murad Masimli <murad.mesimli.99@gmail.com>
2023-08-21 22:52:32 +0400

    router folder

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import React from "react";
 import { RouterProvider } from "react-router-dom";
-import router from 'appRouter'
+import router from 'router/appRouter'
 
 const App = () => {
   return (
